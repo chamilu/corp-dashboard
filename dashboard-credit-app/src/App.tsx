cebc1293880dc1c653d441099ecbec34309bda12
Chamil Udayanga <chamiludayanga28@gmail.com>
2024-06-29 20:08:53 +0800

    [#3, #4, #5] Added initial frontend structure

---
+++ dashboard-credit-app/src/App.tsx
@@ -0,0 +1,11 @@
+import React, { ReactElement } from "react";
+
+const App = () => {
+    return (
+        <div>
+            <h1>This is Credit App</h1>
+        </div>
+    );
+};
+
+export default App;
---
+++ dashboard-shell-app/webpack.config.js
@@ -0,0 +1,47 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const config = (env, { mode }) => {
+    const isProduction = mode === "production";
+
+    return {
+        mode,
+        entry: path.join(__dirname, "src", "index.tsx"),
+        
+        resolve: {
+            extensions: [".tsx", ".ts", ".js", ".jsx"],
+        },
+
+        output: {
+            publicPath: "/",
+            filename: "[name].[chunkhash].js",
+            path: path.resolve(__dirname, "dist"),
+        },
+
+        module: {
+            rules: [
+                {
+                    test: /\.(ts|tsx|js|jsx)?$/,
+                    exclude: /node_modules/,
+                    use: "babel-loader",
+                },
+            ],
+        },
+
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: path.join(__dirname, "public", "index.html"),
+                cache: isProduction,
+            }),
+        ],
+
+        devServer: {
+            hot: true,
+            port: 3000,
+            open: true,
+            historyApiFallback: true,
+        },
+    };
+};
+
+module.exports = config;
