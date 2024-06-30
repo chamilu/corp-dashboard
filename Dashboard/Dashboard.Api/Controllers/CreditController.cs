67a4c106644b82d20af248077898c48daa446edb
Chamil Udayanga <chamiludayanga28@gmail.com>
2024-07-01 00:53:24 +0800

    [#12] Implemented credit controller and service

---
+++ Dashboard/Dashboard.Api/Controllers/CreditController.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using Dashboard.Api.Services;
+using System;
+
+namespace Dashboard.Api.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class CreditController : Controller
+    {
+        private readonly IDashboardService dashboardService;
+
+        public CreditController(IDashboardService _dashboardService)
+        {
+            dashboardService = _dashboardService;
+        }
+
+        [HttpGet("credit")]
+        public IActionResult GetCredits([FromQuery] string duration)
+        {
+            try
+            {
+                int monthCount = dashboardService.GetMonthCount(duration);
+                double[] prices = dashboardService.GeneratePrices(monthCount, 10.0, 100.0);
+                return Ok(new { data = prices });
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+    }
+}
