8d87503f64cfb61556a9ab2278ae90eaa1641de2
Chamil Udayanga <chamiludayanga28@gmail.com>
2024-07-01 09:00:26 +0800

    Added unit testing

---
+++ Dashboard/Dashboard.Api.Tests/Controllers/StockControllerTest.cs
@@ -0,0 +1,23 @@
+using Moq;
+using Dashboard.Api.Controllers;
+using Dashboard.Api.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dashboard.Api.Tests;
+
+public class StockControllerTest
+{
+    [Fact]
+    public void Get_ReturnsOk_WhenValueExists()
+    {
+        var mockService = new Mock<IDashboardService>();
+        mockService.Setup(svc => svc.GetMonthCount("6M")).Returns(6);
+        mockService.Setup(svc => svc.GeneratePrices(6, 10.0, 100.0)).Returns([12.5, 11.4, 23.5, 53.4, 65.4, 54.6]);
+
+        var controller = new CreditController(mockService.Object);
+        var result = controller.GetCredits("6M");
+
+        var okResult = Assert.IsType<OkObjectResult>(result);
+        Assert.NotNull(okResult.Value);
+    }
+}
